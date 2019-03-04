63d0bfed6c19777ba9200fa287cf2a463449ec23
yvan <57882634@qq.com>
2019-03-04 12:40:38 +0800

    变量提升的面试题

--- examples/Variable/variable-hoisting.js
+++ examples/Variable/variable-hoisting.js
@@ -2,8 +2,28 @@
  * variable-hoisting 变量提升
  */
 
-test()
+// 变量提升导致的问题
+for (var i = 1; i <= 3; i++) {
+    setTimeout(function () {
+        console.log('inner i = %d', i)
+    }, 0)
+}
+console.log('outer i = %d', i)
 
-function test() {
-  console.log('test')
+// 解决方案一: let
+for (let j = 1; j <= 3; j++) {
+    setTimeout(function () {
+        console.log('j = %d', j)
+    }, 0)
+}
+// console.log(j)
+// ReferenceError: j is not defined
+
+// 解决方案二：IIFE
+for (var m = 1; m <= 3; m++) {
+    (function (m) {
+        setTimeout(function () {
+            console.log('iife m = %d', m)
+        }, 0)
+    })(m)
 }
